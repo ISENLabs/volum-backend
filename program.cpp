452a0a1678c276ff3c26a5cf9d71a692cd81ee13
MathiAs2Pique <45072138+MathiAs2Pique@users.noreply.github.com>
2025-01-14 19:43:53 +0100

    fix(cache): template decl in .h + std::optional impl

--- program.cpp
+++ program.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include "src/utils/debug.hpp"
 #include "src/utils/config.hpp"
+#include "src/utils/cache.hpp"
 #include "src/services/database.hpp"
 #include "src/services/auth.hpp"
 #include "src/services/webserver.hpp"
 #include "src/providers/lazy_auth.hpp"
 #include "src/utils/http.hpp"
 
+using namespace Utils::Cache;
 using Utils::Debug;
 using Utils::Config::Env_Struct;
 using Services::Database;
+
 
 int main(){
     Debug::Log("Volum-Backend starting up...", "MAIN");
@@ -42,6 +45,8 @@ int main(){
 
     Debug::Log("Launching web server ...", "MAIN");
     Services::WebServer ws;
+
+    CacheHandler<int, std::string> _ch(10);
 
     ws.register_routes();
     ws.run_server(env.webserver_port);
--- src/utils/cache.hpp
+++ src/utils/cache.hpp
@@ -13,23 +13,72 @@ namespace Utils::Cache{
  * The current implementation is good for our little project, with ~100s of objects, but MUST NOT be used in production otherwise.
 */
 
+// Get timestamp
+size_t timestamp(){
+    return static_cast<size_t>(std::time(nullptr));
+}
+
 template <typename T> 
 struct Cache_Object{
     std::shared_ptr<T> object; // As it might be destroyed in its base context
     size_t max_life;
 };
 
-template <typename T, typename K>
+template <typename K, typename T>
 class CacheHandler{
 private:
     int _ttl;
     std::map<K, Cache_Object<T>> _elements; // K: key, T: type of the stored object
 
 public:
-    CacheHandler(uint ttl);
-    std::optional<T> get_element(K key); // Returns the value if not expired. If expired, returns null + deletes the element.
-    void store_element(K key, T object);
-    void ping(K key); // Update the TTL for the element
-    void remove(K key);// Remove the element
+    CacheHandler(uint ttl){
+        _ttl = ttl;
+    }
+
+    // Returns the value if not expired. If expired, returns null + deletes the element.
+    std::optional<T> get_element(K key){
+        if(_elements.find(key) != _elements.end()){
+            if(_elements[key].max_life >= timestamp()){
+                _elements[key].max_life = timestamp()+_ttl;
+                return std::optional<T>(*(_elements[key].object));
+            }
+            // too old ? 
+            else{
+                remove(key);
+                return std::nullopt;
+            }
+        }
+        else{
+            return std::nullopt;
+        }
+    }
+    
+    // Update the TTL for the element
+    void ping(K key){
+        // Check if K exists
+        if(_elements.find(key) != _elements.end()){
+            _elements[key].max_life = timestamp() + _ttl;
+        }
+    }
+    
+    // Remove the element
+    void remove(K key){
+        _elements.erase(key);
+    }
+    
+    void store_element(K key, T object){
+        // if the element already exists, just update its object
+        Cache_Object<T> cobject;
+        cobject.object = std::make_shared<T>(object);
+        cobject.max_life = timestamp() + _ttl;
+
+        if(_elements.find(key) != _elements.end()){
+            _elements[key] = cobject;
+        }
+        else
+            _elements.insert({key, cobject});
+    }
+
+
 };
 };
