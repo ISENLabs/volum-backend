52d553bf4f60cf2dc3e354ebe80811b10545fde4
MathiAs2Pique <45072138+MathiAs2Pique@users.noreply.github.com>
2025-01-15 11:39:33 +0100

    fix(proxmox): don't return a reference
    as the object is created on the stack

--- src/proxmox/requests.hpp
+++ src/proxmox/requests.hpp
@@ -13,18 +13,18 @@ namespace Proxmox{
     public:
         Requests();
         ~Requests();
-        rapidjson::Document& response_to_document(Response resp);
+        rapidjson::Document response_to_document(Response resp);
 
-        rapidjson::Document& list_lxcs();
-        rapidjson::Document& get_lxc(uint pct_id);
-        rapidjson::Document& get_lxc_interfaces(uint pct_id);
-        rapidjson::Document& create_lxc(std::string payload);
-        rapidjson::Document& delete_lxc(uint pct_id);
-        rapidjson::Document& stop_lxc(uint pct_id);
-        rapidjson::Document& start_lxc(uint pct_id);
+        rapidjson::Document list_lxcs();
+        rapidjson::Document get_lxc(uint pct_id);
+        rapidjson::Document get_lxc_interfaces(uint pct_id);
+        rapidjson::Document create_lxc(std::string payload);
+        rapidjson::Document delete_lxc(uint pct_id);
+        rapidjson::Document stop_lxc(uint pct_id);
+        rapidjson::Document start_lxc(uint pct_id);
 
         // TODO: Firewall, but we'll do it later
-        rapidjson::Document& get_lxc_firewall(uint pct_id);
-        rapidjson::Document& update_lxc_firewall(uint pct_id, std::string payload);
+        rapidjson::Document get_lxc_firewall(uint pct_id);
+        rapidjson::Document update_lxc_firewall(uint pct_id, std::string payload);
     };
 };
