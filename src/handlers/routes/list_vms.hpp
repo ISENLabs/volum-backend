ed21aaec31eb0ff2ee0c3cd1306a624990916969
MathiAs2Pique <45072138+MathiAs2Pique@users.noreply.github.com>
2025-01-15 10:28:03 +0100

    feat(routes): list vms

--- src/handlers/routes/list_vms.hpp
+++ src/handlers/routes/list_vms.hpp
@@ -1,18 +1,63 @@
 #include "../../utils/debug.hpp"
+#include "../../services/database.hpp"
+#include "../../proxmox/methods.hpp"
 #include "../middlewares/auth.hpp"
+#include <set>
 #include <crow.h>
 #include <iostream>
 
+using Services::Database;
+using namespace Proxmox::Structs;
+
 namespace Handlers::Routes::VMS{
     std::string list_vms(Handlers::Middlewares::Auth::context& ctx){
-        // Check if admin
-
-        // Get Proxmox PCT list
+        if(ctx.user.userId < 1){
+            return "{\"success\":false, \"error\":\"Unauthenticated user\"";
+        }
 
         // Get Mariadb vms list
+        auto& db = Database::getInstance();
+        auto& conn = db.getConnection();
+        std::shared_ptr<sql::PreparedStatement> stmnt(conn->prepareStatement(
+                  ctx.user.is_admin ? "SELECT * FROM volum_vms" : "SELECT * FROM volum_vms WHERE user_id = ?"
+               )
+            );
+        if(!ctx.user.is_admin)
+            stmnt->setInt(1, ctx.user.userId);
+        auto *res = stmnt->executeQuery();
 
-        // Aggregate, to also return the VM state + stats.
-        return "{\"success\":true, \"message\":\"success\", \"data\":{...todo...}}";
+        std::set<uint> vm_ids;
+        while(res->next()){
+            vm_ids.insert(res->getInt(2)); // ctid
+        }
+
+        try{
+            // Get Proxmox PCT list
+            // For one VM
+            if(vm_ids.size() == 1){
+                Proxmox_LXC lxc = Proxmox::Methods::get_lxc(*(vm_ids.begin()));
+                return "{\"success\":true, \"message\":\"success\", \"data\":["+ Converters::lxc_to_json(lxc) +"]}";
+            }
+
+            // Multiple VMs
+            Proxmox_LXCS lxcs = Proxmox::Methods::get_lxcs();
+            // delete unknown lxcs
+            auto it = lxcs.begin();
+            while(it != lxcs.end()){
+                if(vm_ids.find(it->vm_id) == vm_ids.end())
+                    it = lxcs.erase(it);
+                else
+                    it++;
+            }
+
+            std::string _json = Converters::lxcs_to_json(lxcs);
+
+            return "{\"success\":true, \"message\":\"success\", \"data\":"+ _json +"}";
+        }
+        catch(std::exception& ex){
+            Debug::Log("Unable to complete list_vms query: " + std::string(ex.what()), "R-LVM");
+            return "{\"success\":false, \"message\":\"internal error\"";
+        }
     }
 
 };
